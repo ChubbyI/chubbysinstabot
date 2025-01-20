4630a3e02b7e9d4ac3cea4b4cf4e5265f9a23586
Chubby <chukwubuikem.idigo@gmail.com>
2025-01-20 04:59:54 -0500

    virtual environment

--- src/main.py
+++ src/main.py
@@ -2,5 +2,11 @@ import os
 import json
 import urllib.request
 import boto3
-from datetime import datetime, timedelta, timezone
-from dotenv import load_dotenv
+import spotipy
+from flask import Flask
+#from datetime import datetime, timedelta, timezone
+#from dotenv import load_dotenv
+#from instagrapi import client
+
+app = Flask(__name__)
+
